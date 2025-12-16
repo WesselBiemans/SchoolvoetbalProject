1bf6808a7d111c60087d9681b5397e2d5bf9a27f
WesselBiemans <119673590+WesselBiemans@users.noreply.github.com>
2025-12-16 12:08:22 +0100

    User API Controller added
    Finding users to log them in and showing the user information. Mainly used for the ClientApp

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Api\MatchesApiController;
 use App\Http\Controllers\Api\PlayersApiController;
 use App\Http\Controllers\Api\TeamsApiController;
 use App\Http\Controllers\Api\TeamsTournamentApiController;
+use App\Http\Controllers\Api\UserApiController;
 
 
 
@@ -16,6 +17,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::apiResource('users', UserApiController::class);
+Route::get('/users/search/query', [UserApiController::class, 'search']);
 Route::apiResource('tournaments', TournamentApiController::class);
 Route::apiResource('matches', MatchesApiController::class);
 Route::apiResource('players', PlayersApiController::class);
